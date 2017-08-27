6779ed1038aa0aeebd9248bae79ccb262a58ea6d
vya <yavorskyyval@gmail.com>
2017-08-27 11:38:39 +0300

    added radix.v2 lib for project

--- database/redis/redis.go
+++ database/redis/redis.go
@@ -1,8 +1,29 @@
 package redis
 
-import "github.com/garyburd/redigo/redis"
+import (
+	"github.com/8tomat8/SSU-Golang-252-Chat/loger"
+	"github.com/mediocregopher/radix.v2/pool"
+	"github.com/mediocregopher/radix.v2/redis"
+)
 
-func GetRedisConnection() (redis.Conn, error) {
-	return redis.Dial("tcp", ":6379")
+const NETWORK = "tcp"
+const ADDRESS = "localhost:6379"
+const POOL_SIZE = 10
+
+func GetRedisConn(network string, addr string, size int) (conn *redis.Client, err error) {
+	p, err := pool.New(network, addr, size)
+	if err != nil {
+		loger.Log.Error(err)
+	}
+	loger.Log.Print("pool created")
+	conn, err = p.Get()
+	if err != nil {
+		loger.Log.Error(err)
+	}
+	loger.Log.Print("connection established")
+	//resp := conn.Cmd("ping")
+	//fmt.Printf("resi value is %v , type is %T \n", resp, resp )
+	return conn, err
 }
-// just a comment
+
+
