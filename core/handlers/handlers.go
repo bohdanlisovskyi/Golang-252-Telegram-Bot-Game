e0c1d7a3f62d6a547bff6d511ae934ef4675dfbf
Greckas <greckasus@gmail.com>
2017-09-10 15:20:07 +0300

    Menu Structure

--- core/handlers/handlers.go
+++ core/handlers/handlers.go
@@ -17,15 +17,85 @@ func StartGame(message *tbot.Message) {
 	time.Sleep(500 * time.Millisecond)
 
 	buttons := [][]string{
-		{"Menu"},
+		{"Buildings"},
 	}
-	message.ReplyKeyboard("Buttons example", buttons)
+	message.ReplyKeyboard("Here is  your planet, build the future!", buttons)
 }
 
 func ShowMenu(message *tbot.Message) {
 
 	buttons := [][]string{
-		{"Units", "Building"},
+		{"Research Centre", "Mines", "City Centre", "Cosmodrome", "Rating", "Planet history"},
 	}
-	message.ReplyKeyboard("Menu", buttons)
+	message.ReplyKeyboard("Buildings", buttons)
+}
+
+func ShowResearchMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Tutorial", "Help", "Planet search", "Buildings"},
+	}
+	message.ReplyKeyboard("Research Centre", buttons)
+}
+
+func ShowMinesMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Metal mine", "Crystal mine", "Buildings"},
+	}
+	message.ReplyKeyboard("Mines", buttons)
+}
+func ShowMetalMinesMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Upgrade", "Employ people", "Buildings", "Status"},
+	}
+	message.ReplyKeyboard("Metal mine", buttons)
+}
+func ShowCrystalMinesMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Upgrade", "Employ people", "Buildings", "Status"},
+	}
+	message.ReplyKeyboard("Crystal mine", buttons)
+}
+
+func ShowCityCentreMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Upgrade", "Status", "Buildings"},
+	}
+	message.ReplyKeyboard("City Centre", buttons)
+}
+
+func ShowCosmodromeMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Fleet", "Dockyard", "Buildings"},
+	}
+	message.ReplyKeyboard("Cosmodrome", buttons)
+}
+
+func ShowFleetMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Set new fleet", "Edit existing group", "Status", "Buildings"},
+	}
+	message.ReplyKeyboard("Fleet", buttons)
+}
+
+func ShowDockyardMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Build new ships", "Status", "Buildings"},
+	}
+	message.ReplyKeyboard("Dockyard", buttons)
+}
+
+func ShowRatingMenu(message *tbot.Message) {
+
+	buttons := [][]string{
+		{"Resourses used", "Population", "Buildings"},
+	}
+	message.ReplyKeyboard("Rating", buttons)
 }
--- core/router/listener.go
+++ core/router/listener.go
@@ -12,7 +12,43 @@ var routes = Routes{
 		HandlerFunc: handlers.StartGame,
 	},
 	Route{
-		Path:        "Menu",
+		Path:        "Buildings",
 		HandlerFunc: handlers.ShowMenu,
+	},
+	Route{
+		Path:        "Research Centre",
+		HandlerFunc: handlers.ShowResearchMenu,
+	},
+	Route{
+		Path:        "Mines",
+		HandlerFunc: handlers.ShowMinesMenu,
+	},
+	Route{
+		Path:        "Metal mine",
+		HandlerFunc: handlers.ShowMetalMinesMenu,
+	},
+	Route{
+		Path:        "Crystal mine",
+		HandlerFunc: handlers.ShowCrystalMinesMenu,
+	},
+	Route{
+		Path:        "City Centre",
+		HandlerFunc: handlers.ShowCityCentreMenu,
+	},
+	Route{
+		Path:        "Cosmodrome",
+		HandlerFunc: handlers.ShowCosmodromeMenu,
+	},
+	Route{
+		Path:        "Fleet",
+		HandlerFunc: handlers.ShowFleetMenu,
+	},
+	Route{
+		Path:        "Dockyard",
+		HandlerFunc: handlers.ShowDockyardMenu,
+	},
+	Route{
+		Path:        "Rating",
+		HandlerFunc: handlers.ShowRatingMenu,
 	},
 }
