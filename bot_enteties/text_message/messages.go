223ec9d4c53f1f3a57ec9b35d8ed44184d9de224
Bohdan <www.lbohdan@gmail.com>
2017-09-11 09:49:24 +0300

    update core arch

---
+++ bot_enteties/text_message/messages.go
@@ -0,0 +1,7 @@
+package text_message
+
+const MessageAboutGame = "This game about bla bla bla :)"
+const EnterPlanetName = "*Please Enter Planet Name With command* ```\n\n``` `/name {planet name}`"
+const PlanetCreated = "Planet %s!, was created. \n*Congratulation!!!*"
+const NextStep = "*Please choose what you want to do next?\n*"
+const TutorialText = "Tutorial text"
