8fb5c2e798f8fc371e7a18249b6d5d98a5d4f793
vya <yavorskyyval@gmail.com>
2017-09-12 22:19:21 +0300

    tests for updating functionality

---
+++ updatingService/Updates_test.go
@@ -0,0 +1,21 @@
+package updatingService
+
+import "testing"
+
+func TestUpdateCitycenter(t *testing.T) {
+
+}
+
+func TestUpdateIronmine(t *testing.T) {
+}
+
+func TestUpdateCrystalmine(t *testing.T) {
+}
+
+func TestUpdateCosmodrome(t *testing.T) {
+}
+
+func TestUpdateShip(t *testing.T) {
+}
+
+
