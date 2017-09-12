3611e28ed5d58fff618897438c3bad468530c40d
vya <yavorskyyval@gmail.com>
2017-09-12 22:19:34 +0300

    tests for updating functionality

--- updatingService/Updates_test.go
+++ updatingService/Updates_test.go
@@ -7,15 +7,19 @@ func TestUpdateCitycenter(t *testing.T) {
 }
 
 func TestUpdateIronmine(t *testing.T) {
+
 }
 
 func TestUpdateCrystalmine(t *testing.T) {
+
 }
 
 func TestUpdateCosmodrome(t *testing.T) {
+
 }
 
 func TestUpdateShip(t *testing.T) {
+
 }
 
 
