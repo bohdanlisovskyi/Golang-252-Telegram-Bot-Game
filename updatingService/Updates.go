791f938425fcac17532b127800a1f0577b1efc45
vya <yavorskyyval@gmail.com>
2017-09-12 22:01:16 +0300

    functionality for updating

---
+++ updatingService/Updates.go
@@ -0,0 +1,21 @@
+package updatingService
+
+func UpdateCitycenter()  {
+	
+}
+
+func UpdateIronmine()  {
+	
+}
+
+func UpdateCrystalmine()  {
+	
+}
+
+func UpdateCosmodrome()  {
+	
+}
+
+func UpdateShip()  {
+	
+}
