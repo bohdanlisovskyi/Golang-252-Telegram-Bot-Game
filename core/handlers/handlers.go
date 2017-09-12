d7a9a5cd8a70ed226206d509a74a413f8aae47d5
Bohdan <www.lbohdan@gmail.com>
2017-09-12 11:20:05 +0300

    upd library in glide, and add some flow

--- core/handlers/handlers.go
+++ core/handlers/handlers.go
@@ -6,7 +6,7 @@ import (
 	"github.com/bohdanlisovskyi/Golang-252-Telegram-Bot-Game/bot_enteties/buttons"
 	"github.com/bohdanlisovskyi/Golang-252-Telegram-Bot-Game/bot_enteties/emojies"
 	"github.com/bohdanlisovskyi/Golang-252-Telegram-Bot-Game/bot_enteties/text_message"
-	"github.com/yanzay/tbot"
+	"github.com/bohdanlisovskyi/telegrambot/tbot"
 )
 
 func EchoHandler(message *tbot.Message) {
@@ -49,4 +49,58 @@ func GameTutorial(message *tbot.Message) {
 	time.Sleep(3 * time.Second)
 
 	message.ReplyKeyboard("Buildings", buttons.AllBuildings, tbot.ResizeKeyboard)
+}
+
+func DataCenter(message *tbot.Message) {
+
+	message.ReplyKeyboard("DataCenter", buttons.DataCenter, tbot.ResizeKeyboard)
+}
+
+func SkipTutorial(message *tbot.Message) {
+
+	message.Reply(text_message.SkipToturial)
+
+	message.ReplyKeyboard("Buildings", buttons.AllBuildings, tbot.ResizeKeyboard)
+}
+
+func AllBuildings(message *tbot.Message) {
+
+	message.ReplyKeyboard("Buildings", buttons.AllBuildings, tbot.ResizeKeyboard)
+}
+
+func DataCenterStatus(message *tbot.Message) {
+	//TODO show some status
+	status := "some status here"
+
+	message.Reply(status)
+}
+
+func DataCenterToturial(message *tbot.Message) {
+
+	message.Reply(text_message.TutorialText)
+}
+
+func DataCenterSearchPlanet(message *tbot.Message) {
+
+	//TODO code for search planet
+
+	message.Reply("Some text as a result to search planet: Show list of neighbor!!!")
+
+	message.ReplyKeyboard("Search Planet", buttons.AfterSearchButton, tbot.ResizeKeyboard)
+}
+
+func DataCenterSearchListOfPlanet(message *tbot.Message) {
+
+	//TODO code for getting list of planet
+
+	listOfPlanet := "some list of planet"
+
+	message.Reply(listOfPlanet)
+
+	message.ReplyKeyboard("Go to Cosmodrom", buttons.SearchListPlanet, tbot.ResizeKeyboard)
+}
+
+func CosmodromMenu(message *tbot.Message) {
+
+	message.ReplyKeyboard("Cosmodrom", buttons.CosmodromMenu, tbot.ResizeKeyboard)
 }
