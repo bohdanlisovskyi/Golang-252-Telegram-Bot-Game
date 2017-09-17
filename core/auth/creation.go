05b93bde9c222a717186e63959f592a47cac57d6
Greckas <greckasus@gmail.com>
2017-09-17 20:47:09 +0300

    Merge remote-tracking branch 'origin/master' into greckas
    # Conflicts:
    #	core/handlers/handlers.go
    #	core/router/listener.go

--- core/auth/creation.go
+++ core/auth/creation.go
@@ -1,35 +1,61 @@
 package auth
 
 import (
-	"math"
-
+	"github.com/bohdanlisovskyi/Golang-252-Telegram-Bot-Game/algo"
 	"github.com/bohdanlisovskyi/Golang-252-Telegram-Bot-Game/database"
 	"github.com/bohdanlisovskyi/telegrambot/tbot"
 )
 
-func PlaceNewPlanet(amountOfPlanets int) (int, int) {
-	if amountOfPlanets == 0 {
-		return 0, 0
-	}
-	flatOfSpiral := 1.1
-	cornerCoefficient := 16.0
-	fAmountOfPlanets := float64(amountOfPlanets)
-	//Archimedean spiral equation with some  modifications
-	xCoordinate := flatOfSpiral * (fAmountOfPlanets + 1) * math.Cos(cornerCoefficient/math.Abs(math.Log10(fAmountOfPlanets+1))*math.Sqrt(fAmountOfPlanets+1))
-	yCoordinate := flatOfSpiral * (fAmountOfPlanets + 1) * math.Sin(cornerCoefficient/math.Abs(math.Log10(fAmountOfPlanets+1))*math.Sqrt(fAmountOfPlanets+1))
-	return int(xCoordinate), int(yCoordinate)
-}
-
-func UserCreation(message *tbot.Message) (string, error) {
+func UserCreation(message *tbot.Message) error {
 	db, err := database.GetPostgresConnection()
 	if err != nil {
-		return "", err
+		return err
 	}
 	plan := &database.Planet{}
 	db.Last(plan)
 	last_id := plan.Id
-	x, y := PlaceNewPlanet(last_id)
+	x, y := algo.PlaceNewPlanet(last_id)
 	planet := database.Planet{PlanetName: message.Vars["planet_name"], UserName: message.From, XCoordinate: x, YCoordinate: y, IsActive: true, IsLoaded: true}
 	db.Create(&planet)
-	return "all ok", nil
+	citycenter := database.Citycenter{
+		PlanetName:      message.Vars["planet_name"],
+		Level:           1,
+		PeopleAmount:    10,
+		PeopleMaxAmount: 100,
+		CitycenterKPI:   2,
+	}
+	db.Create(&citycenter)
+	resource := database.Resource{
+		PlanetName:    message.Vars["planet_name"],
+		IronAmount:    500,
+		CrystalAmount: 500,
+	}
+	db.Create(&resource)
+	iron_mine := database.IronMine{
+		PlanetName:      message.Vars["planet_name"],
+		Level:           0,
+		MineKPI:         2,
+		PeopleAmount:    0,
+		PeopleMaxAmount: 0,
+	}
+	db.Create(&iron_mine)
+	crystal_mine := database.CrystalMine{
+		PlanetName:      message.Vars["planet_name"],
+		Level:           0,
+		MineKPI:         2,
+		PeopleAmount:    0,
+		PeopleMaxAmount: 0,
+	}
+	db.Create(&crystal_mine)
+	cosmodrome := database.Cosmodrome{
+		PlanetName:      message.Vars["planet_name"],
+		Level:           0,
+		CosmodrKPI:      2,
+		ShipAmount:      0,
+		ShipMaxAmount:   0,
+		FleetsAmount:    0,
+		FleetsMaxamount: 0,
+	}
+	db.Create(&cosmodrome)
+	return nil
 }
