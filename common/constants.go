0acb0e45bd92b1087bd446ee6da91183540a23d7
YAWAL <yavorskyyval@gmail.com>
2017-09-27 15:31:50 +0300

    added file with constants

---
+++ common/constants.go
@@ -0,0 +1,15 @@
+package common
+
+const (
+	MAX_LEVEL                = 3
+	LEVEL1_PEOPLE_MAX_AMOUNT = 10
+	LEVEL2_PEOPLE_MAX_AMOUNT = 20
+	LEVEL3_PEOPLE_MAX_AMOUNT = 30
+	LEVEL1_KPI               = 1.1
+	LEVEL2_KPI               = 1.2
+	LEVEL3_KPI               = 1.2
+	LEVEL2_UPDATE_TIME       = 259200 //72*60*60
+	LEVEL3_UPDATE_TIME       = 172800 //48*60*60
+
+)
+
